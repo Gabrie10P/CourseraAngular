132dd55f370e8d1620cfb7c6c9661a7c9ec46f99
Gabrielvallep <gabrielv_034@hotmail.com>
2022-12-22 23:38:31 -0600

    Actualizacion
    Single Page Application Part 2

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { DishService } from '../services/dish.service';
+
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 const DISH = {
   id: '0',
@@ -54,7 +58,17 @@ const DISH = {
 export class DishdetailComponent implements OnInit{
   @Input()
   dish = DISH;
-  constructor(){}
-  ngOnInit(){}
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location){}
+
+  ngOnInit(){
+    const id = + this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id.toString());
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 
 }
